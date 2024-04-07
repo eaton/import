730d6379b7bad99566579ac34649a4f06ba797d9
Jeff Eaton <jeff@eaton.fyi>
2024-04-07 03:50:28 -0500

    Random mdb files

---
+++ src/datasets/access.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import MDBReader from "mdb-reader";
+import jetpack from "@eatonfyi/fs-jetpack";
+import { NdJson } from "@eatonfyi/serializers";
+import path from 'path';
+
+jetpack.setSerializer('.ndjson', NdJson);
+
+const input = jetpack.dir(process.env.INPUT_MDB ?? '');
+const cache = jetpack.dir('./cache');
+
+for (const db of input.find({ matching: '*.mdb' })) {
+  const reader = new MDBReader(input.read(db, 'buffer') as Buffer);
+  const dbName = path.parse(db).name;
+  for (const tableName of reader.getTableNames()) {
+    const table = reader.getTable(tableName);
+    if (table.rowCount) {
+      cache.dir(dbName).write(`${table.name}.ndjson`, table.getData());
+    }
+  }
+}
+
+
+
