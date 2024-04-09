1429b1e17eb61e2f67f5565844a8731b3424eb3e
Jeff Eaton <jeff@eaton.fyi>
2024-04-09 08:45:58 -0500

    Bookmark import consolidation

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import { MovableTypeImport } from "./blogs/movabletype.js";
 import { TumblrImport } from "./blogs/tumblr.js";
+import { BookmarkImport } from "./bookmarks/bookmarks.js";
 
 const tb = new TumblrImport({
   blogList: ['govertainment', 'plf', 'cmswhoops'],
 });
-tb.output.remove();
+//tb.output.remove();
 
 const mt = new MovableTypeImport({
   mysql_db: 'mt2005',
   userList: [4],
   blogList: [3, 4]
 });
-mt.output.remove();
+//mt.output.remove();
 
+const bm = new BookmarkImport({ 
+  base: "/Volumes/home/migration",
+});
+bm.output.remove();
 
-await tb.runImport();
-await mt.runImport();
+//await tb.runImport();
+//await mt.runImport();
+await bm.runImport();
