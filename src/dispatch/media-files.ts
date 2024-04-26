bdb6f2e261fb9291a2c079e8840ea59edf00d064
Jeff Eaton <jeff@eaton.fyi>
2024-04-25 22:44:30 -0500

    Shuffling things a bit to make room for later copies

--- src/dispatch/media-files.ts
+++ src/dispatch/media-files.ts
@@ -24,6 +24,9 @@ export class MediaFileImport extends BaseImport {
   }
   
   override async finalize() {
+
+    /** First, grab blogs and static files from personal sites I used for image hosting. */
+
     this.input.copy('blogs/predicatenet/files', this.output.path('predicate'));
     this.log.info('Copied predicate.net files');
 
@@ -41,15 +44,21 @@ export class MediaFileImport extends BaseImport {
     this.input.copy('blogs/angrylittletree-jekyll/files', this.output.path('alt'), { overwrite: true });
     this.log.info('Copied angrylittletree media');
 
+
+    /** Manually yoined images and assets for reprinted articles, static microsites, etc. */
+
+    this.input.copy('articles/images', this.output.path('articles'));
+    this.log.info('Copied article reprint images');
+
+
+
+    /** Social media accounts */
+
     this.input.copy('blogs/tumblr/files', this.output.path('tumblr'));
     this.log.info('Copied tumblr media');
 
     this.input.copy('media/skitch', this.output.path('skitch'));
     this.log.info('Copied skitch backup');
-
-    this.input.copy('articles/images', this.output.path('articles'));
-    this.log.info('Copied article reprint images');
-
 
     const twitterInput = jetpack.dir(process.env.INPUT_TWITTER ?? 'input');
     const twitterOutput = this.output.dir('twitter');
@@ -58,30 +67,32 @@ export class MediaFileImport extends BaseImport {
       const archive = new TwitterArchive(buffer!, {
         ignore: ['ad', 'block', 'dm', 'moment', 'mute']
       });
-      await archive.ready();
-      
-      for (const t of archive.tweets.all) {
-        for (const me of t.extended_entities?.media ?? []) {
-          const variant = me.video_info?.variants
-            ?.filter((v) => v.content_type === 'video/mp4')
-            .pop();
-          const filename = path
-            .parse(variant?.url ?? me.media_url_https)
-            .base.split('?')[0];
-          
-            if (!twitterOutput.exists(filename)) {
-              try {
-                const ab = await archive.medias.fromTweetMediaEntity(me, true);
-                const buffer = Buffer.from(ab as ArrayBuffer);
-                twitterOutput.write(filename, buffer);
-              } catch (err: unknown) {
-                this.log.error(`Couldn't read ${filename} from archive`);
+      try {
+        await archive.ready();
+        for (const t of archive.tweets.all) {
+          for (const me of t.extended_entities?.media ?? []) {
+            const variant = me.video_info?.variants
+              ?.filter((v) => v.content_type === 'video/mp4')
+              .pop();
+            const filename = path
+              .parse(variant?.url ?? me.media_url_https)
+              .base.split('?')[0];
+            
+              if (!twitterOutput.exists(filename)) {
+                try {
+                  const ab = await archive.medias.fromTweetMediaEntity(me, true);
+                  const buffer = Buffer.from(ab as ArrayBuffer);
+                  twitterOutput.write(filename, buffer);
+                } catch (err: unknown) {
+                  this.log.error(err, `Couldn't read ${filename} from archive`);
+                }
               }
             }
-          }
+        }
+        this.log.info(`Copied media for ${archive.user.screen_name}`);
+      } catch (err: unknown) {
+        this.log.error(err, `Couldn't load archive`);
       }
-      this.log.info(`Copied media for ${archive.user.screen_name}`);
-
     }
   }
 }
